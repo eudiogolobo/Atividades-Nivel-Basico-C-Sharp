894ce4fb467b7080773cf41af275c379086ca076
Diogo Lobo <diogolobo444@gmail.com>
2023-11-25 19:31:18 -0300

    Commit calculadora

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+using System.Xml.Schema;
+
+
+while (true){
+
+Console.WriteLine("Digite 1 para somar ou 2 para subtarir ou 3 para multiplicar ou 4 para dividir ou 0 para sair.");
+
+var indice = int.Parse(Console.ReadLine());
+
+if (indice==0){
+    System.Environment.Exit(-1);
+}
+
+var numero1 = double.Parse(Console.ReadLine());
+
+var numero2 = double.Parse(Console.ReadLine());
+
+double resultado = 0;
+
+
+
+switch (indice) {
+    case 1:
+    switch (resultado = numero1 + numero2)
+    {}break;
+
+    case 2:
+    switch (resultado = numero1 - numero2)
+    {}break;
+        
+        
+    case 3:
+    switch (resultado = numero1 * numero2)
+    {}break;
+
+    case 4:
+    switch (resultado = numero1 / numero2)
+    {}break;
+
+    case 0:
+    System.Environment.Exit(-1);
+    {}break;
+            
+                       
+}
+Console.WriteLine(Convert.ToString(resultado));
+
+}
+
+
+ 
